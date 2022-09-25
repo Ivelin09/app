8c68303aaecc62da17dc61f326d1d245b1dac005
Ivelin09 <galinaliobomirova@gmail.com>
2022-09-25 15:59:35 +0300

    gitignore

--- src/register.erl
+++ src/register.erl
@@ -1,25 +1,16 @@
 -module(register).
 
--export([init/2, allowed_methods/2, home/2, content_types_provided/2,
-         content_types_accepted/2, add_entry/2, resource_exists/2, delete_resources/2]).
+-export([init/2, allowed_methods/2, resource_exists/2, delete_resources/2]).
 
 
 allowed_methods(Req, State) ->
     {[<<"GET">>, <<"POST">>, <<"DELETE">>], Req, State}.
 
-content_types_provided(Req, State) ->
-    {[{{<<"application">>, <<"json">>, []}, home}], Req, State}.
-
-content_types_accepted(Req, State) ->
-    {[{<<"application/json">>, add_entry}], Req, State}.
-
-add_entry(Req, State) ->
-    {ok, Req, State}.
-
 resource_exists(Req, State) ->
     Headers = cowboy_req:headers(Req),
     BasicAuth = lists:nth(2, string:lexemes(maps:get(<<"authorization">>, Headers), " ")),
-    [Username, Password] = string:lexemes(erlang:binary_to_list(base64:decode(<<"YXNkOjEyMw==">>)), ":"),
+    [Username, Password] = string:lexemes(erlang:binary_to_list(base64:decode(BasicAuth)), ":"),
+    io:format("~p ~p", [Username, Password]),
     case cowboy_req:method(Req) of 
         "POST" ->
             {stop, cowboy_req:reply(200, Req), State};
@@ -29,9 +20,6 @@ resource_exists(Req, State) ->
 
 delete_resources(Req, State) ->
     {true, Req, State}.
-
-home(Req, State) ->
-    {"asdasd", Req, State}.
 
 init(Req0, State) ->
     {cowboy_rest, Req0, State}.
