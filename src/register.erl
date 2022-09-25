5cc6555ed3c23d48ca3f96a5aee4207b6c92aa5f
Ivelin09 <galinaliobomirova@gmail.com>
2022-09-25 13:17:06 +0300

    Basic Auth

--- src/register.erl
+++ src/register.erl
@@ -1,4 +1,4 @@
--module(registere).
+-module(register).
 
 -export([init/2, allowed_methods/2, home/2, content_types_provided/2,
          content_types_accepted/2, add_entry/2, resource_exists/2, delete_resources/2]).
@@ -17,14 +17,14 @@ add_entry(Req, State) ->
     {ok, Req, State}.
 
 resource_exists(Req, State) ->
-    io:format("Here"),
     Headers = cowboy_req:headers(Req),
-    BasicAuth = maps:get(<<"authorization">>, Headers),
+    BasicAuth = lists:nth(2, string:lexemes(maps:get(<<"authorization">>, Headers), " ")),
+    [Username, Password] = string:lexemes(erlang:binary_to_list(base64:decode(<<"YXNkOjEyMw==">>)), ":"),
     case cowboy_req:method(Req) of 
         "POST" ->
-            {stop, cowboy_req:reply(200), State};
+            {stop, cowboy_req:reply(200, Req), State};
         _ ->
-            {stop, cowboy_req:reply(200), State}
+            {stop, cowboy_req:reply(200, Req), State}
     end.
 
 delete_resources(Req, State) ->
